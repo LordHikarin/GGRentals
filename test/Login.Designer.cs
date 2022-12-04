006b566f7b5de0b5d18e91192c4cd10cda59a87f
StoneCentaur2 <orlando.el311@gmail.com>
2022-12-04 13:34:10 -0700

    cambios en el login

--- test/Login.Designer.cs
+++ test/Login.Designer.cs
@@ -32,11 +32,11 @@
             this.sPanel1 = new MarDeCortezDsk.UserControlls.SPanel();
             this.login_BTN = new MarDeCortezDsk.UserControlls.BotonRedondo();
             this.label3 = new System.Windows.Forms.Label();
-            this.circularImagen1 = new test.UserControlls.CircularImagen();
             this.TxtBoxContraseña = new CustomControls.RJControls.RJTextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.TxtBoxUsuario = new CustomControls.RJControls.RJTextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.circularImagen1 = new test.UserControlls.CircularImagen();
             this.sPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.circularImagen1)).BeginInit();
             this.SuspendLayout();
@@ -53,9 +53,10 @@
             this.sPanel1.Controls.Add(this.TxtBoxUsuario);
             this.sPanel1.Controls.Add(this.label2);
             this.sPanel1.Edge = 20;
-            this.sPanel1.Location = new System.Drawing.Point(25, 12);
+            this.sPanel1.Location = new System.Drawing.Point(33, 15);
+            this.sPanel1.Margin = new System.Windows.Forms.Padding(4);
             this.sPanel1.Name = "sPanel1";
-            this.sPanel1.Size = new System.Drawing.Size(268, 445);
+            this.sPanel1.Size = new System.Drawing.Size(357, 548);
             this.sPanel1.TabIndex = 14;
             // 
             // login_BTN
@@ -69,9 +70,10 @@
             this.login_BTN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.login_BTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.login_BTN.ForeColor = System.Drawing.Color.White;
-            this.login_BTN.Location = new System.Drawing.Point(30, 371);
+            this.login_BTN.Location = new System.Drawing.Point(40, 457);
+            this.login_BTN.Margin = new System.Windows.Forms.Padding(4);
             this.login_BTN.Name = "login_BTN";
-            this.login_BTN.Size = new System.Drawing.Size(205, 40);
+            this.login_BTN.Size = new System.Drawing.Size(273, 49);
             this.login_BTN.TabIndex = 17;
             this.login_BTN.Text = "Ingresar";
             this.login_BTN.TextColor = System.Drawing.Color.White;
@@ -84,28 +86,12 @@
             this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(26, 235);
+            this.label3.Location = new System.Drawing.Point(35, 289);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(209, 20);
+            this.label3.Size = new System.Drawing.Size(252, 25);
             this.label3.TabIndex = 16;
             this.label3.Text = "____________________";
-            // 
-            // circularImagen1
-            // 
-            this.circularImagen1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
-            this.circularImagen1.BorderColor = System.Drawing.Color.RoyalBlue;
-            this.circularImagen1.BorderColor2 = System.Drawing.Color.HotPink;
-            this.circularImagen1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
-            this.circularImagen1.BorderSize = 2;
-            this.circularImagen1.GradientAngle = 50F;
-            this.circularImagen1.Image = global::test.Properties.Resources.LogoGG;
-            this.circularImagen1.Location = new System.Drawing.Point(66, 21);
-            this.circularImagen1.Name = "circularImagen1";
-            this.circularImagen1.Size = new System.Drawing.Size(135, 135);
-            this.circularImagen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.circularImagen1.TabIndex = 14;
-            this.circularImagen1.TabStop = false;
-            this.circularImagen1.Click += new System.EventHandler(this.circularImagen1_Click);
             // 
             // TxtBoxContraseña
             // 
@@ -116,15 +102,15 @@
             this.TxtBoxContraseña.BorderSize = 2;
             this.TxtBoxContraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtBoxContraseña.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.TxtBoxContraseña.Location = new System.Drawing.Point(36, 297);
-            this.TxtBoxContraseña.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtBoxContraseña.Location = new System.Drawing.Point(48, 366);
+            this.TxtBoxContraseña.Margin = new System.Windows.Forms.Padding(5);
             this.TxtBoxContraseña.Multiline = false;
             this.TxtBoxContraseña.Name = "TxtBoxContraseña";
-            this.TxtBoxContraseña.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.TxtBoxContraseña.PasswordChar = false;
+            this.TxtBoxContraseña.Padding = new System.Windows.Forms.Padding(13, 9, 13, 9);
+            this.TxtBoxContraseña.PasswordChar = true;
             this.TxtBoxContraseña.PlaceholderColor = System.Drawing.Color.DarkGray;
             this.TxtBoxContraseña.PlaceholderText = "Contraseña";
-            this.TxtBoxContraseña.Size = new System.Drawing.Size(199, 33);
+            this.TxtBoxContraseña.Size = new System.Drawing.Size(265, 43);
             this.TxtBoxContraseña.TabIndex = 15;
             this.TxtBoxContraseña.Texts = "";
             this.TxtBoxContraseña.UnderlinedStyle = false;
@@ -135,9 +121,10 @@
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(26, 168);
+            this.label1.Location = new System.Drawing.Point(35, 207);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.Size = new System.Drawing.Size(197, 25);
             this.label1.TabIndex = 10;
             this.label1.Text = "Nombre de Usuario";
             // 
@@ -150,15 +137,15 @@
             this.TxtBoxUsuario.BorderSize = 2;
             this.TxtBoxUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TxtBoxUsuario.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.TxtBoxUsuario.Location = new System.Drawing.Point(36, 198);
-            this.TxtBoxUsuario.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtBoxUsuario.Location = new System.Drawing.Point(48, 244);
+            this.TxtBoxUsuario.Margin = new System.Windows.Forms.Padding(5);
             this.TxtBoxUsuario.Multiline = false;
             this.TxtBoxUsuario.Name = "TxtBoxUsuario";
-            this.TxtBoxUsuario.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.TxtBoxUsuario.Padding = new System.Windows.Forms.Padding(13, 9, 13, 9);
             this.TxtBoxUsuario.PasswordChar = false;
             this.TxtBoxUsuario.PlaceholderColor = System.Drawing.Color.DarkGray;
             this.TxtBoxUsuario.PlaceholderText = "usuario";
-            this.TxtBoxUsuario.Size = new System.Drawing.Size(199, 33);
+            this.TxtBoxUsuario.Size = new System.Drawing.Size(265, 43);
             this.TxtBoxUsuario.TabIndex = 15;
             this.TxtBoxUsuario.Texts = "";
             this.TxtBoxUsuario.UnderlinedStyle = false;
@@ -169,21 +156,40 @@
             this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(76)))), ((int)(((byte)(76)))));
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(26, 268);
+            this.label2.Location = new System.Drawing.Point(35, 330);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.Size = new System.Drawing.Size(155, 25);
             this.label2.TabIndex = 11;
             this.label2.Text = "Clave de Inicio";
             // 
+            // circularImagen1
+            // 
+            this.circularImagen1.BorderCapStyle = System.Drawing.Drawing2D.DashCap.Flat;
+            this.circularImagen1.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.circularImagen1.BorderColor2 = System.Drawing.Color.HotPink;
+            this.circularImagen1.BorderLineStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            this.circularImagen1.BorderSize = 2;
+            this.circularImagen1.GradientAngle = 50F;
+            this.circularImagen1.Location = new System.Drawing.Point(88, 23);
+            this.circularImagen1.Margin = new System.Windows.Forms.Padding(4);
+            this.circularImagen1.Name = "circularImagen1";
+            this.circularImagen1.Size = new System.Drawing.Size(180, 180);
+            this.circularImagen1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.circularImagen1.TabIndex = 14;
+            this.circularImagen1.TabStop = false;
+            this.circularImagen1.Click += new System.EventHandler(this.circularImagen1_Click);
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkCyan;
-            this.ClientSize = new System.Drawing.Size(319, 469);
+            this.ClientSize = new System.Drawing.Size(425, 577);
             this.Controls.Add(this.sPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Login";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
