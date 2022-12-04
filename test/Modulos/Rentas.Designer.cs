06817c1d4bd5d963bb6b81f1b9969e1964db7e2b
LordHikarin <Andres_G20@outlook.com>
2022-12-04 16:47:52 -0700

    Cambios en pantalla de Rentas

--- test/Modulos/Rentas.Designer.cs
+++ test/Modulos/Rentas.Designer.cs
@@ -45,52 +45,47 @@
             // 
             // inquilino_id_TBX
             // 
-            this.inquilino_id_TBX.Location = new System.Drawing.Point(5, 59);
-            this.inquilino_id_TBX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.inquilino_id_TBX.Location = new System.Drawing.Point(13, 48);
             this.inquilino_id_TBX.Name = "inquilino_id_TBX";
-            this.inquilino_id_TBX.Size = new System.Drawing.Size(232, 22);
+            this.inquilino_id_TBX.Size = new System.Drawing.Size(175, 20);
             this.inquilino_id_TBX.TabIndex = 27;
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(293, 44);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Location = new System.Drawing.Point(224, 48);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 32;
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(293, 112);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker2.Location = new System.Drawing.Point(224, 97);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker2.TabIndex = 33;
             this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
             // 
             // costo_TBX
             // 
-            this.costo_TBX.Location = new System.Drawing.Point(291, 176);
-            this.costo_TBX.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.costo_TBX.Location = new System.Drawing.Point(13, 239);
+            this.costo_TBX.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.costo_TBX.Name = "costo_TBX";
             this.costo_TBX.ReadOnly = true;
-            this.costo_TBX.Size = new System.Drawing.Size(120, 22);
+            this.costo_TBX.Size = new System.Drawing.Size(91, 20);
             this.costo_TBX.TabIndex = 35;
             // 
             // Departamento_id_TBX
             // 
-            this.Departamento_id_TBX.Location = new System.Drawing.Point(8, 114);
-            this.Departamento_id_TBX.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Departamento_id_TBX.Location = new System.Drawing.Point(13, 97);
             this.Departamento_id_TBX.Name = "Departamento_id_TBX";
-            this.Departamento_id_TBX.Size = new System.Drawing.Size(232, 22);
+            this.Departamento_id_TBX.Size = new System.Drawing.Size(175, 20);
             this.Departamento_id_TBX.TabIndex = 37;
             // 
             // new_BTN
             // 
-            this.new_BTN.Location = new System.Drawing.Point(51, 222);
-            this.new_BTN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.new_BTN.Location = new System.Drawing.Point(38, 180);
             this.new_BTN.Name = "new_BTN";
-            this.new_BTN.Size = new System.Drawing.Size(100, 28);
+            this.new_BTN.Size = new System.Drawing.Size(75, 23);
             this.new_BTN.TabIndex = 38;
             this.new_BTN.Text = "Nuevo";
             this.new_BTN.UseVisualStyleBackColor = true;
@@ -98,10 +93,9 @@
             // 
             // del_BTN
             // 
-            this.del_BTN.Location = new System.Drawing.Point(159, 222);
-            this.del_BTN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.del_BTN.Location = new System.Drawing.Point(119, 180);
             this.del_BTN.Name = "del_BTN";
-            this.del_BTN.Size = new System.Drawing.Size(100, 28);
+            this.del_BTN.Size = new System.Drawing.Size(75, 23);
             this.del_BTN.TabIndex = 39;
             this.del_BTN.Text = "Borrar";
             this.del_BTN.UseVisualStyleBackColor = true;
@@ -110,59 +104,64 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(4, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.Size = new System.Drawing.Size(138, 20);
             this.label1.TabIndex = 40;
             this.label1.Text = "Codigo Inquilino";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(4, 95);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(3, 74);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(140, 16);
+            this.label2.Size = new System.Drawing.Size(185, 20);
             this.label2.TabIndex = 41;
             this.label2.Text = "Codigo Departamento";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(291, 23);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(206, 28);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.Size = new System.Drawing.Size(128, 20);
             this.label3.TabIndex = 42;
             this.label3.Text = "Fecha Llegada";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(293, 84);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(206, 74);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 16);
+            this.label4.Size = new System.Drawing.Size(114, 20);
             this.label4.TabIndex = 43;
             this.label4.Text = "Fecha Salida";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(293, 154);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(3, 217);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 16);
+            this.label5.Size = new System.Drawing.Size(101, 20);
             this.label5.TabIndex = 44;
             this.label5.Text = "Costo Total";
             // 
             // btn_Contrato
             // 
-            this.btn_Contrato.Location = new System.Drawing.Point(159, 267);
-            this.btn_Contrato.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_Contrato.Location = new System.Drawing.Point(119, 217);
+            this.btn_Contrato.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Contrato.Name = "btn_Contrato";
-            this.btn_Contrato.Size = new System.Drawing.Size(117, 57);
+            this.btn_Contrato.Size = new System.Drawing.Size(88, 46);
             this.btn_Contrato.TabIndex = 45;
             this.btn_Contrato.Text = "Generar contrato";
             this.btn_Contrato.UseVisualStyleBackColor = true;
@@ -170,8 +169,9 @@
             // 
             // Rentas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(28)))), ((int)(((byte)(54)))));
             this.Controls.Add(this.btn_Contrato);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -185,9 +185,8 @@
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.inquilino_id_TBX);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Rentas";
-            this.Size = new System.Drawing.Size(605, 594);
+            this.Size = new System.Drawing.Size(454, 483);
             this.Load += new System.EventHandler(this.Rentas_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
