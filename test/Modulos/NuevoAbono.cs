f0f6961c714ebbb5efb3c1255af810422d541d87
StoneCentaur2 <orlando.el311@gmail.com>
2022-12-07 01:39:56 -0700

    se añadio una clase para convertir los numeros a letras

--- test/Modulos/NuevoAbono.cs
+++ test/Modulos/NuevoAbono.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using objWord = Microsoft.Office.Interop.Word;
 using System.Windows.Forms;
 
 namespace test.Modulos
@@ -14,6 +15,7 @@ namespace test.Modulos
     public partial class NuevoAbono : UserControl
     {
         Conexion.SQL conexion = new Conexion.SQL();
+        Conversion c = new Conversion();
         public delegate void volverDelegate();
         public event volverDelegate Volver;
         public NuevoAbono()
@@ -32,6 +34,29 @@ namespace test.Modulos
             if (deuda > 0)
             {
                 conexion.CMD("UPDATE rentas SET deudas = '" + deuda + " ', WHERE id_inquilino =" + user_TBX.Text.ToString() + "'");
+                string NumerosALetras = c.enletras(cantidad_TBX.Texts);
+                //string ruta = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);//la ruta especificada es el escritorio
+                objWord.Application objAplicacion = new objWord.Application();
+                //creación del documento
+                //objWord.Document objDocumento = objAplicacion.Documents.Add();
+                //Parrafo 1
+                //objWord.Paragraph objTitulo = objDocumento.Content.Paragraphs.Add(Type.Missing);
+                object oMissing = System.Reflection.Missing.Value;
+                object oEndOfDoc = "\\endofdoc";
+
+                //Start Word and create a new document.
+                objWord._Application oWord;
+                objWord._Document oDoc;
+                oWord = new objWord.Application();
+                oWord.Visible = true;
+                object oTemplate = "C:\\Users\\orlan\\Music\\RECIBO.docx"; //Se debe de especificar correctamente su ubicación
+                oDoc = oWord.Documents.Open(ref oTemplate, ref oMissing,
+                    ref oMissing, ref oMissing);
+                object Lugar = "Lugar"; //Se debe añadir el marcador en la plantilla de word
+                objWord.Range place = oDoc.Bookmarks.get_Item(ref Lugar).Range;
+                place.Text = "Puerto Peñasco, Sonora, México";// lo que se desea modificar
+                object rangoP = place;
+                oDoc.Bookmarks.Add("Lugar", ref rangoP); //Marcador que modificará
             }
             else
             {
